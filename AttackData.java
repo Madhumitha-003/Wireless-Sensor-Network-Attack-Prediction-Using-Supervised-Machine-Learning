8f7fb0a6171da859d73d9598468bc46638e7c17e
Madhumitha-003 <146418447+Madhumitha-003@users.noreply.github.com>
2025-04-19 13:18:26 +0530

    Create AttackData.java

---
+++ AttackData.java
@@ -0,0 +1,10 @@
+package com.wsn.model;
+
+public class AttackData {
+    private double packetLoss;
+    private double delay;
+    private double energy;
+    private double throughput;
+
+    // Getters and setters
+}
